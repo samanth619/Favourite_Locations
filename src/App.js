6ec754b82b62c155ea002b011948891b3abca4b4
Samanth Baraju <samanthreddy619@gmail.com>
2024-03-10 17:16:07 +0530

    Embedded google map with search fr a place functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from "react";
 import Map from "./components/Map";
+import SearchLocation from "./components/SearchLocation";
 import "./App.css";
 
 function App() {
+  const [selectedLocation, setSelectedLocation] = useState({
+    lat: 14.0169,
+    lng: 74.3272,
+  });
+
+  const handleSelectedLocation = (location) => {
+    setSelectedLocation(location);
+  };
+
   return (
     <div className="App">
       <h1>Mark your favourite locations</h1>
-      <Map />
+      {/* <SearchLocation setSelectedLocation={setSelectedLocation} /> */}
+      <Map
+        selectedLocation={selectedLocation}
+        handleSelectedLocation={handleSelectedLocation}
+      />
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,24 +1,74 @@
-import { useState } from "react";
-import GoogleMapReact from "google-map-react";
+import React, { useRef, useCallback } from "react";
+import {
+  GoogleMap,
+  MarkerF,
+  StandaloneSearchBox,
+  LoadScript,
+} from "@react-google-maps/api";
+import "./Map.css";
 
-const Map = ({ center, zoom }) => {
+const Map = ({ selectedLocation, handleSelectedLocation }) => {
+  const searchBoxRef = useRef();
+  const mapRef = useRef();
+  const libraries = ["places"];
+
+  const handlePlacesChanged = () => {
+    if (searchBoxRef.current) {
+      const places = searchBoxRef.current.getPlaces();
+      console.log("🚀 ~ handlePlacesChanged ~ places:", places);
+      if (!Array.isArray(places) || places.length < 0) return;
+      let lat = places[0].geometry.location.lat();
+      let lng = places[0].geometry.location.lng();
+      handleSelectedLocation({ lat, lng });
+    }
+  };
+
   return (
     <div className="map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-      ></GoogleMapReact>
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+        libraries={libraries}
+      >
+        <GoogleMap
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          center={selectedLocation}
+          zoom={13}
+          onLoad={(map) => (mapRef.current = map)}
+          options={{ streetViewControl: false }}
+        >
+          <MarkerF
+            position={selectedLocation}
+            icon={"https://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+          />
+          {true && (
+            <StandaloneSearchBox
+              onLoad={(ref) => {
+                searchBoxRef.current = ref;
+              }}
+              onPlacesChanged={handlePlacesChanged}
+            >
+              <input
+                type="text"
+                placeholder="Customized your placeholder"
+                className="search-box"
+              />
+            </StandaloneSearchBox>
+          )}
+        </GoogleMap>
+      </LoadScript>
+      {false && (
+        <>
+          <div className="outer-frame f1"></div>
+          <div className="outer-frame f2"></div>
+          <div className="outer-frame f3"></div>
+          <div className="outer-frame f4"></div>
+        </>
+      )}
     </div>
   );
-};
-
-Map.defaultProps = {
-  center: {
-    lat: 8.0844,
-    lng: 77.5495,
-  },
-  zoom: 6,
 };
 
 export default Map;
